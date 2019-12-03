e88af64deda81804e913530bc7ba314154767da9
Seymur <mustafav.seymur@gmail.com>
2019-12-03 17:16:15 +0200

    Updates

---
+++ precache-manifest.ea8bcf5a84f74494ae6e7b65cf6e58dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb49c3da18f0b31d20718931ac968884",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "2c784423a3d56988a6b6",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.7c70754d.chunk.css"
+  },
+  {
+    "revision": "0807ec32d485d4ccc8b1",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.923594be.chunk.js"
+  },
+  {
+    "revision": "2c784423a3d56988a6b6",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.4b07e0a7.chunk.js"
+  },
+  {
+    "revision": "50060ce28f153c3693da",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3b1a272b.js"
+  }
+]);
